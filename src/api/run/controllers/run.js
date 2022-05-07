64aa1b4a95b633d47d9a1736e02f946c91f00beb
Pascal Vaccaro <pascal.vaccaro@gmail.com>
2022-05-07 11:03:18 +0200

    feat: add filters to the creation endpoint of runs and runners

--- src/api/run/controllers/run.js
+++ src/api/run/controllers/run.js
@@ -6,4 +6,22 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::run.run');
+module.exports = createCoreController('api::run.run', ({ strapi }) => ({
+  async create(ctx) {
+    const { numberSign, race } = ctx.request.body.data;
+    const [data] = await strapi.entityService.findMany('api::run.run', {
+      filters: {
+        numberSign,
+      },
+      populate: {
+        race: {
+          filters: {
+            id: race
+          }
+        }
+      },
+    });
+
+    return data ? { data } : super.create(ctx);
+  }
+}));
--- src/api/runner/controllers/runner.js
+++ src/api/runner/controllers/runner.js
@@ -6,4 +6,17 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::runner.runner');
+module.exports = createCoreController('api::runner.runner', ({ strapi }) => ({
+  async create(ctx) {
+    const { firstName, lastName, email } = ctx.request.body.data;
+    const [data] = await strapi.entityService.findMany('api::runner.runner', {
+      filters: {
+        $and: [
+          { firstName, lastName, email }
+        ]
+      }
+    });
+
+    return data ? { data } : super.create(ctx);
+  }
+}));
