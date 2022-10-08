aaff856d2cef63ae63481a847aacf284a9044966
Pascal Vaccaro <pascal.vaccaro@gmail.com>
2022-10-08 21:29:33 +0200

    chore: move deployment to Droplet

--- config/database.ts
+++ config/database.ts
@@ -2,10 +2,17 @@ import path from 'path';
 
 export default ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'pascal'),
+      password: env('DATABASE_PASSWORD', ''),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+      }
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
